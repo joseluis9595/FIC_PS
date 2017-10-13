eecd377033c9f6d9486ac8883e27086a61cecee4
Matias <matias.cubillac@udc.es>
2017-10-13 16:13:33 +0200

    Creado proyecto

---
+++ src/android/Project/app/src/main/java/es/udc/psi1718/project/MainAct.java
@@ -0,0 +1,13 @@
+package es.udc.psi1718.project;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainAct extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
